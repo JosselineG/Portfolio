50f67dd5be2e6a7f2a022982380de5d98bfdb4fd
Josseline <guardadojosseline@gmail.com>
2022-08-07 21:29:47 -0400

    added components

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './NavBar.css' 
+
+function NavBar() {
+
+
+    return (
+        <nav className="nav">
+            <h1>navigationBar</h1>
+            
+        </nav>
+
+    );
+}
+export default NavBar;
