d2168567a3aaf260f4b8fada074a7a18e9a41f49
Josseline <guardadojosseline@gmail.com>
2022-08-12 00:14:00 -0400

    handleEvents on contactMe component added

--- src/Components/ContactMe/ContactMe.js
+++ src/Components/ContactMe/ContactMe.js
@@ -2,60 +2,86 @@ import React from 'react';
 import './ContactMe.css'
 import SendIcon from '@mui/icons-material/Send';
 
-function ContactMe() {
+class ContactMe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { fullName: "", email: "", message: "" };
+  }
+
+  handleChange = (e) => {
+
+    this.setState({ ...this.state, [e.target.name]: e.target.value })
+    
+  }
+
+  handleSubmit = (e) => {
 
 
+    alert("message sent!" + this.state.fullName + " | " + this.state.email + " | " + this.state.message)
+    this.setState({ fullName: "", email: "", message: "" })
+    e.preventDefault();
+  }
 
 
-  return (
-    <div className='cont' id='ContactMe'>
-      <h1>Contact</h1>
+  render() {
+    return (
+      <div className='cont' id='ContactMe'>
+        <h1>Contact</h1>
 
-      <div className='fo'>
-        <div className='formBox'>
+        <div className='fo'>
+          <div className='formBox'>
 
-          <form >
-            <label>
-              Name:
-            </label>
-            <input
-              type={"text"} />
+            <form >
+              <label htmlFor='fullName'>
+                Name:
+              </label>
+              <input
+                type={"text"}
+                name='fullName'
+                onChange={(e) => this.handleChange(e)}
+                value={this.state.fullName} />
 
-            <label>
-              Email:
-            </label>
-            <input
-              type={"email"} />
+              <label htmlFor='email'>
+                Email:
+              </label>
+              <input
+                type={"email"}
+                name='email'
+                onChange={(e) => this.handleChange(e)}
+                value={this.state.email} />
 
-            <label>
-              Message:
-            </label>
-            <textarea
-              type={"text"}
-           /*  placeholder="Message" */ />
+              <label htmlFor='message'>
+                Message:
+              </label>
+              <textarea
+                type={"text"}
+                name="message"
+                onChange={(e) => this.handleChange(e)}
+                value={this.state.message} />
 
-            <div className="btonContainer">
-              <button type='submit' >
-                Send
-                <SendIcon sx={{ fontSize: 15 }} />
-              </button>
-            </div>
+              <div className="btonContainer">
+                <button type='submit'
+                  onClick={(e) => this.handleSubmit(e)}>
+                  Send
+                  <SendIcon sx={{ fontSize: 15 }} />
+                </button>
+              </div>
 
-          </form>
+            </form>
+          </div>
+
+
+          <div className="info">
+            <h3>If you have any questions or want to say hello, contact me by filling out the form or emailing me at
+              guardadojosseline@gmail.com</h3>
+          </div>
+
         </div>
 
 
-        <div className="info">
-          <h3>If you have any questions or want to say hello, contact me by filling out the form or emailing me at
-            guardadojosseline@gmail.com</h3>
-        </div>
 
       </div>
-
-
-
-    </div>
-  );
+    );
+  }
 }
-
 export default ContactMe;
