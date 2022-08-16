9fc5285bfdb3a2d30acd930e8fe0848f96cda167
Josseline <guardadojosseline@gmail.com>
2022-08-15 21:32:40 -0400

    Added to the project and aboutMe components

--- src/Components/AboutMe/aboutMe.js
+++ src/Components/AboutMe/aboutMe.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import './AboutMe.css'
+import { SiJavascript } from 'react-icons/si';
+import './AboutMe.css';
+import { DiReact } from "react-icons/di";
+import { DiCss3 } from "react-icons/di";
+import { AiFillHtml5 } from "react-icons/ai";
+
+
 
 function AboutMe() {
 
@@ -7,7 +13,15 @@ function AboutMe() {
 
   return (
     <div className='abt' id="AboutMe">
-   <h1>About</h1>
+      
+
+  <h1>About</h1>
+   <p> I am a recent graduated student, with a passion for web development. </p>
+   <SiJavascript style={{color: "yellow" ,width:"50px", height:"50px"}}/>
+   <DiReact style={{color: "cyan" ,width:"50px", height:"50px", backgroundColor:"black"}}/>
+    <DiCss3 style={{color: "blue" ,width:"50px", height:"50px"}} />
+    <AiFillHtml5 style={{color: "orange" ,width:"50px", height:"50px"}} />
+   
    </div>
   );
 }
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,5 +1,7 @@
+import { color } from '@mui/system';
 import React from 'react';
 import './Projects.css'
+
 
 function Projects() {
 
@@ -7,8 +9,43 @@ function Projects() {
 
   return (
     <div className='prj' id='Projects'>
-   <h1>Projects</h1>
-   </div>
+      <h1>Projects</h1>
+
+      <div className='prjContainer'>
+
+      
+        <div className='prj1'>
+          <a href='https://github.com/JosselineG/design-pad'>
+          <h1>Design Pad</h1>
+          {/* <p>This is a group project</p> */}
+          </a>
+        </div>
+   
+        <div className='prj2'>
+         
+          {/*  <p> Weather app using openweathermap API</p> */}
+          <a href='https://github.com/JosselineG/My2Project'>
+          <h1>Weather App</h1>
+           </a>
+        </div>
+
+        <div className='prj3'>
+  
+          {/*  <p> This is my first portfolio</p> */}
+          <a href='https://github.com/JosselineG/josselineg.github.io'>
+          <h1>First Portfolio</h1></a>
+        </div>
+
+        <div className='prj4'>
+          <h1>Mechanic Website</h1>
+          {/*  <p> This is a website I created for a small business</p> */}
+        </div>
+
+      </div>
+
+
+
+    </div>
   );
 }
 
