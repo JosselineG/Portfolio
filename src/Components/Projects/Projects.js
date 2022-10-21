29be1478b34c91ff5dec4d76da1760b73a201c61
Josseline <guardadojosseline@gmail.com>
2022-10-21 12:51:10 -0400

    Working Portfolio

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -12,33 +12,35 @@ function Projects() {
 
       <div className='prjContainer'>
 
-             
+
         <a className='prj1' href='https://github.com/JosselineG/design-pad'>
-         
+
           Design Pad
-          {/* <p>Group project</p> */}
-          
         </a>
-       
-       
-     
-          {/*  <p> Weather app using openweathermap API</p> */}
-          <a className='prj2' href='https://github.com/JosselineG/My2Project'>
+
+
+
+        {/*  <p> Weather app using openweathermap API</p> */}
+        <a className='prj2' href='https://github.com/JosselineG/My2Project'>
+
           Weather App
-           </a>
-           
-       
-              {/*  <p> This is my first portfolio</p> */}
-          <a className='prj3' href='https://github.com/JosselineG/josselineg.github.io'>
-          First Portfolio</a>
-         
-         
+        </a>
+
+
+        {/*  <p> This is my first portfolio</p> */}
+        <a className='prj3' href='https://github.com/JosselineG/josselineg.github.io'>
+          Portfolio
+
+        </a>
+
+
         <a className='prj4' href="https://github.com/JosselineG/Small-Business-Website">
           Small Business Website
-          {/*  <p> This is a website I created for a small business</p> */}
+
+
         </a>
-          
-       
+
+
       </div>
 
 
