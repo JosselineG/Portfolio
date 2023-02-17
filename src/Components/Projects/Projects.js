f5cce3ed0f9a91e810c69d31378115315c55bd7b
Josseline <guardadojosseline@gmail.com>
2023-02-17 16:56:28 -0500

    update: added a project

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -13,43 +13,16 @@ function Projects() {
       <div className='prjContainer'>
 
 
-        <a className='prj1' href='https://github.com/JosselineG/design-pad'>
-
-          Design Pad
-        </a>
-
-
-
-        {/*  <p> Weather app using openweathermap API</p> */}
-        <a className='prj2' href='https://github.com/JosselineG/My2Project'>
-
-          Weather App
-        </a>
-
-
-        {/*  <p> This is my first portfolio</p> */}
-        <a className='prj3' href='https://github.com/JosselineG/josselineg.github.io'>
-          Older Portfolio
-
-        </a>
-
-
-        <a className='prj4' href="https://github.com/JosselineG/Small-Business-Website">
-          Small Business Website
-
-
-        </a>
-
-        
-        <a className='prj5' href="https://github.com/JosselineG/Wordle-Clone">
-          Wordle Clone ESP
-
-
-        </a>
+        <a className='prj1' href='https://github.com/JosselineG/design-pad'> Design Pad </a>
+        <a className='prj2' href='https://github.com/JosselineG/My2Project'> Weather App </a>
+        <a className='prj3' href='https://github.com/JosselineG/josselineg.github.io'> Older Portfolio </a>
+        <a className='prj4' href="https://github.com/JosselineG/Small-Business-Website"> Small Business Website </a>
+        <a className='prj5' href="https://github.com/JosselineG/Wordle-Clone"> Wordle Clone ESP </a>
+        <a className='prj6' href="https://github.com/JosselineG/Ecommerce-Template"> Food Ecommerce Store </a>  
 
 
 
-      </div>
+       </div>
 
 
 
