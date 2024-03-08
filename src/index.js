b9fc9f37f5194af1703474e198d10219b2adb495
Josseline Guardado <guardadojosseline@gmail.com>
2024-03-07 22:19:11 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import SinglePageApp from './Components/SinglePageApp/SinglePageApp';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
